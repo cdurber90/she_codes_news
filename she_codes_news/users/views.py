51a35392b030781aae37212a82307bc22a7a8c32
Charlotte Durber <cdurber@gmail.com>
2023-12-09 12:21:57 +0800

    links to stories on user profile

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,4 +1,6 @@
 from typing import Any
+from django.db import models
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views import generic
@@ -21,4 +23,17 @@ class UserAccountView(generic.DetailView):
         context = super().get_context_data(**kwargs)
         user = self.get_object()
         context['user_stories'] = NewsStory.objects.filter(author=user)
+        return context
+    
+class AuthorDetailView(generic.DetailView):
+    model = CustomUser
+    template_name = 'news/Index.html'
+    context_object_name = 'author'
+
+    def get_object(self, *args, **kwargs):
+        return get_object_or_404(CustomUser, username = self.kwargs['username'])
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_stories'] = NewsStory.objects.filter(author__id=self.object.id)
         return context
