e1cd84c202e326f3e81a97703226d05581c8b464
Charlotte Durber <cdurber@gmail.com>
2023-12-20 10:22:56 +0800

    final add

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+# from .views import IndexView, StoryView, AddStoryView, UpdateStoryView, DeleteStoryView
 
 app_name = 'news'
 
@@ -7,5 +8,8 @@ urlpatterns = [
     # path('', views.IndexView.as_view(), name='index')
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
-    path('add-story/', views.AddStoryView.as_view(), name='newStory')
+    path('add-story/', views.AddStoryView.as_view(), name='newStory'),
+    path('edit/<int:pk>', views.UpdateStoryView.as_view(), name='update_story'),
+    path('edit/<int:pk>/delete', views.DeleteStoryView.as_view(), name='delete_story'),
+    # path('<int:pk>/comments/', views.AddCommentsView.as_view(), name='addComment'),
 ]
--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -4,6 +4,8 @@ from .models import NewsStory
 from .forms import StoryForm
 
 
+
+
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
     context_object_name = "all_stories"
@@ -15,6 +17,7 @@ class IndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:4]
+        context['all_stories'] = NewsStory.objects.all().order_by('-pub_date')[2:]
         return context
     
 class StoryView(generic.DetailView):
@@ -31,3 +34,36 @@ class AddStoryView(generic.CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class UpdateStoryView(generic.UpdateView):
+    model=NewsStory
+    template_name = "news/update.html"
+    fields = ['title','category', 'image_url', 'pub_date', 'content']
+    success_url = reverse_lazy('news:index')
+
+class DeleteStoryView(generic.DeleteView):
+    model = NewsStory
+    template_name = "news/delete.html"
+    success_url = reverse_lazy('news:index')
+    
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
+
+
+    
+
+# class AddCommentsView(generic.CreateView):
+#     form_class = CommentForm
+    
+
+#     def get(self,request, *args, **kwargs):
+#         return redirect ("news:story", pk=self.kwargs.get("pk"))
+    
+#     def form_valid(self, form):
+#         form.instance.author = self.request.user
+#         pk = self.kwargs.get("pk")
+#         form.instance.story = get_object_or_404 (NewsStory,pk=pk)
+#         return super().form_valid(form)
+    
+#     def get_success_url(self):
+#         return reverse_lazy('news:story', kwargs={'pk':self.kwargs.get('pk')})
